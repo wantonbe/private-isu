c6317cf19ed15fc07f810249e688f908bf86d387
wantonbe <wantonbe@gmail.com>
2022-07-09 17:33:34 +0900

    slow-query-logもローテンションする

--- webapp/rotate.sh
+++ webapp/rotate.sh
@@ -3,3 +3,6 @@
 docker compose exec nginx mv /var/log/nginx/access.log /var/log/nginx/access.log.`date +%Y%m%d-%H%M%S`
 docker compose exec nginx /usr/sbin/nginx -s reopen
 
+docker compose exec mysql rm /var/log/mysql/mysql-slow.log
+docker compose exec mysql mysqladmin -u root --password=root flush-logs
+
